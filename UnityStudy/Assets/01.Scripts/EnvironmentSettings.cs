6cd7d2af780a4d2f305763477b70e54e7a507b5d
2ha1aa <rlarmsfbf7415@gmail.com>
2026-03-20 14:24:54 +0900

    [FIX] 불필요 클래스 삭제

--- UnityStudy/Assets/01.Scripts/EnvironmentSettings.cs
+++
@@ -1,8 +0,0 @@
-public static class EnvironmentSettings
-{
-    private const bool IS_DIAGONAL_MOVE_ALLOWED = true;
-    public static bool IsDiagonalMoveAllowed => IS_DIAGONAL_MOVE_ALLOWED;
-
-    private const float DIAGONAL_MOVE_COST_MULTIPLIER = 1.4f;
-    public static float DiagonalMoveCostMultiplier => DIAGONAL_MOVE_COST_MULTIPLIER;
-}
